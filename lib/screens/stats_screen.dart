cb212e550304abf5009a1650fa4c2d0320341c39
luciamonetta <l.monetta8@studenti.unisa.it>
2026-05-30 18:49:54 +0200

    M

--- lib/screens/stats_screen.dart
+++ lib/screens/stats_screen.dart
@@ -1357,24 +1357,3 @@ class _EmptyCard extends StatelessWidget {
   }
 }
 
-class _MiniStat extends StatelessWidget {
-  final String label;
-  final String value;
-  final Color color;
-  const _MiniStat(
-      {required this.label, required this.value, required this.color});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(label,
-            style: const TextStyle(fontSize: 12, color: Colors.grey)),
-        const SizedBox(height: 4),
-        Text(value,
-            style: TextStyle(
-                fontSize: 18, fontWeight: FontWeight.bold, color: color)),
-      ],
-    );
-  }
-}
